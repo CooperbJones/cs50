ad8b3fe4f220473dd03710fd35e959d232093976
CooperbJones <31903550+CooperbJones@users.noreply.github.com>
2017-09-12 16:08:43 -0500

    Create water.c

---
+++ water.c
@@ -0,0 +1,13 @@
+int GetBottles(int minutes)
+{
+    int bottles;
+    bottles = ((1.5 * 128) / 16) * minutes;
+    return bottles;
+}
+
+int main(void)
+{
+    printf("minutes: ");
+    int minutes = GetInt();
+    printf("bottles: %d\n", GetBottles(minutes));
+}
