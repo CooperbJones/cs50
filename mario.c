d71358150ddbdfa8ba65856ead9e537b5705200e
CooperbJones <31903550+CooperbJones@users.noreply.github.com>
2017-09-12 16:11:03 -0500

    Update mario.c

--- mario.c
+++ mario.c
@@ -15,15 +15,15 @@ int main(void)
 
 void PrintPyramid(int height)
 {
-    // the number of rows to print
+   
     for (int i = 0; i < height; i++)
     {
-        // print the spaces
+        
         for (int j = 0; j < (height - i - 1); j++)
         { 
             printf(" ");
         }
-        // print the hashes
+       
         for (int k = 0; k < i + 2; k++)
         {
             printf("#");
