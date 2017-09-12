97ac199c02b2974f725b9b675ba5d6d0eb5d53ee
CooperbJones <31903550+CooperbJones@users.noreply.github.com>
2017-09-12 16:07:09 -0500

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,33 @@
+void PrintPyramid(int);
+
+int main(void)
+{
+    int height;
+    do
+    {
+        printf("height: ");
+        height = GetInt();
+    }
+    while(height < 0  || height > 23);
+
+    PrintPyramid(height);
+}
+
+void PrintPyramid(int height)
+{
+    // the number of rows to print
+    for (int i = 0; i < height; i++)
+    {
+        // print the spaces
+        for (int j = 0; j < (height - i - 1); j++)
+        { 
+            printf(" ");
+        }
+        // print the hashes
+        for (int k = 0; k < i + 2; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
